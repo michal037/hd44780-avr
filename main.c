8bde0a052435f9dc58c7111aebaa7be7f9886ad4
Michał Kozakiewicz <michalkozakiewicz3@gmail.com>
2018-06-20 18:18:09 +0200

    update

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ uint8_t EEMEM upArrow[] = {32,4,14,31,4,4,4,4};
  *
  * Remember to define F_CPU= for each file!
  * Remember to configure pins in lcd.h file!
- * Remember to upload data to the EEPROM memory after flashing!
+ * Remember to upload data to the EEPROM memory after flashing, for the relevant functions.
  */
 int main(void)
 {
